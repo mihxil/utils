8c3251cf0009884c202a079406dd062a47c3d574
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-11-11 12:37:05 +0100

    More testing.

--- mihxil-functional/src/test/java/org/meeuw/functional/EquivalenceTest.java
+++ mihxil-functional/src/test/java/org/meeuw/functional/EquivalenceTest.java
@@ -16,15 +16,19 @@ class EquivalenceTest {
 
 
     @Test
-    public void and() {
+    public void equivalence() {
         String a1 = "a";
         String a2 = "a";
         Equivalence<String> equivalence = Objects::equals;
         assertThat(equivalence.test(a1, a2)).isTrue();
         Predicate<String> predicate = equivalence.predicate(a1);
         assertThat(predicate.test(a2)).isTrue();
+
         assertThat(predicate).isEqualTo(predicate);
+        assertThat(predicate).isEqualTo(equivalence.predicate("a"));
         assertThat(predicate).isNotEqualTo(equivalence.predicate("b"));
+        assertThat(predicate.equals("a")).isFalse();
+        assertThat(predicate.equals(null)).isFalse();
         assertThat(predicate.toString()).isEqualTo("equivalent to a");
 
         assertThat(Stream.of(a1, a2).allMatch(predicate)).isTrue();
