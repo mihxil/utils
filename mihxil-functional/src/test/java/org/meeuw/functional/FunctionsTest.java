390fe22c1c42f21c4ce66fb2f2fbe70ae44641b1
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-11-23 13:05:02 +0100

    Increasing coverage.

--- mihxil-functional/src/test/java/org/meeuw/functional/FunctionsTest.java
+++ mihxil-functional/src/test/java/org/meeuw/functional/FunctionsTest.java
@@ -20,10 +20,10 @@ class FunctionsTest {
         Function<Object, String> x = Functions.always("x");
         assertThat(x.apply(new Object())).isEqualTo("x");
 
-        assertThat(x).isEqualTo(Functions.always("x"));
-        assertThat(x).isEqualTo(x);
-        assertThat(x).isNotEqualTo(Functions.always("y"));
-        assertThat(x).isNotEqualTo(new Object());
+        assertThat(x.equals(Functions.always("x"))).isTrue();
+        assertThat(x.equals(x)).isTrue();
+        assertThat(x.equals(Functions.always("y"))).isFalse();
+        assertThat(x.equals(new Object())).isFalse();
 
 
 
@@ -40,9 +40,9 @@ class FunctionsTest {
     @Test
     void biAlways() {
         assertThat(Functions.biAlways("x").apply("y", "z")).isEqualTo("x");
-        assertThat(Functions.biAlways("x")).isEqualTo(Functions.biAlways("x"));
-        assertThat(Functions.biAlways("x")).isNotEqualTo(Functions.biAlways("y"));
-        assertThat(Functions.biAlways("x")).isNotEqualTo(Functions.always("y"));
+        assertThat(Functions.biAlways("x").equals(Functions.biAlways("x"))).isTrue();
+        assertThat(Functions.biAlways("x").equals(Functions.biAlways("y"))).isFalse();
+        assertThat(Functions.biAlways("x").equals(Functions.always("y"))).isFalse();
         assertThat(Functions.biAlways("x").toString()).isEqualTo("always x");
         assertThat(Functions.biAlways("x", "X").toString()).isEqualTo("X");
     }
@@ -51,9 +51,9 @@ class FunctionsTest {
     @Test
     void triAlways() {
         assertThat(Functions.triAlways("x").apply("y", "z", 1)).isEqualTo("x");
-        assertThat(Functions.triAlways("x")).isEqualTo(Functions.triAlways("x"));
-        assertThat(Functions.triAlways("x")).isNotEqualTo(Functions.triAlways("y"));
-        assertThat(Functions.triAlways("x")).isNotEqualTo(Functions.always("y"));
+        assertThat(Functions.triAlways("x").equals(Functions.triAlways("x"))).isTrue();
+        assertThat(Functions.triAlways("x").equals(Functions.triAlways("y"))).isFalse();
+        assertThat(Functions.triAlways("x").equals(Functions.always("y"))).isFalse();
         assertThat(Functions.triAlways("x").toString()).isEqualTo("always x");
         assertThat(Functions.triAlways("x", "X").toString()).isEqualTo("X");
     }
@@ -61,16 +61,15 @@ class FunctionsTest {
     @Test
     void quadriAlways() {
         assertThat(Functions.quadriAlways("x").apply("y", "z", 1, 2)).isEqualTo("x");
-        assertThat(Functions.quadriAlways("x")).isEqualTo(Functions.quadriAlways("x"));
-        assertThat(Functions.quadriAlways("x")).isNotEqualTo(Functions.quadriAlways("y"));
-        assertThat(Functions.quadriAlways("x")).isNotEqualTo(Functions.always("y"));
+        assertThat(Functions.quadriAlways("x").equals(Functions.quadriAlways("x"))).isTrue();
+        assertThat(Functions.quadriAlways("x").equals(Functions.quadriAlways("y"))).isFalse();
+        assertThat(Functions.quadriAlways("x").equals(Functions.always("y"))).isFalse();
         assertThat(Functions.quadriAlways("x").toString()).isEqualTo("always x");
         assertThat(Functions.quadriAlways("x", "X").toString()).isEqualTo("X");
     }
 
 
     final Function<Float, String> mono = Object::toString;
-
 
     @Test
     void monoWithArg1() {
@@ -80,16 +79,12 @@ class FunctionsTest {
     @Test
     void monoIgnoreArg1() {
         assertThat(Functions.ignoreArg1(mono).apply(new Object(), 1.0f)).isEqualTo("1.0");
-
     }
-
 
     @Test
     void monoIgnoreArg2() {
         assertThat(Functions.ignoreArg2(mono).apply(1.0f, new Object())).isEqualTo("1.0");
-
     }
-
 
     static class Bi implements BiFunction<String, Double, String> {
         @Override
@@ -123,11 +118,11 @@ class FunctionsTest {
         Function<Double, String> s = withArg1(bi, "s");
         assertThat(s.apply(2.0)).isEqualTo("s+2.0");
 
-        assertThat(s).isEqualTo(withArg1(bi, "s"));
-        assertThat(s).isEqualTo(s);
+        assertThat(s.equals(withArg1(bi, "s"))).isTrue();
+        assertThat(s.equals(s)).isTrue();
 
-        assertThat(s).isNotEqualTo(withArg1(bi, "t"));
-        assertThat(s).isNotEqualTo(new Object());
+        assertThat(s.equals(withArg1(bi, "t"))).isFalse();
+        assertThat(s.equals(new Object())).isFalse();
 
         assertThat(s.hashCode()).isEqualTo(withArg1(bi, "s").hashCode());
         assertThat(s.hashCode()).isNotEqualTo(withArg1(bi, "t").hashCode());
@@ -139,14 +134,15 @@ class FunctionsTest {
     void biWithArg2() {
         assertThat(withArg2(bi, 2.0).apply("s")).isEqualTo("s+2.0");
 
-        assertThat(withArg2(bi, 2.0)).isEqualTo(withArg2(bi, 2.0));
-        assertThat(withArg2(bi, 2.0)).isNotEqualTo(withArg2(bi, 3.0));
+        assertThat(withArg2(bi, 2.0).equals(withArg2(bi, 2.0))).isTrue();
+        assertThat(withArg2(bi, 2.0).equals(withArg2(bi, 3.0))).isFalse();
     }
 
     @Test
     void biIgnoreArg3() {
         assertThat(ignoreArg3(bi).apply("a", 2.0, new Object())).isEqualTo("a+2.0");
     }
+
     @Test
     void biIgnoreArg2() {
         assertThat(ignoreArg2(bi).apply("a", new Object(), 2.0)).isEqualTo("a+2.0");
@@ -157,17 +153,14 @@ class FunctionsTest {
         assertThat(ignoreArg1(bi).apply(new Object(), "a", 2.0)).isEqualTo("a+2.0");
     }
 
-
     @Test
     void triIgnoreArg4() {
         assertThat(ignoreArg4(tri).apply("a", 1.0, 2.0f, new Object())).isEqualTo("a+1.0+2.0");
-
     }
 
     @Test
     void triIgnoreArg3() {
         assertThat(ignoreArg3(tri).apply("a", 1.0,new Object(), 2.0f)).isEqualTo("a+1.0+2.0");
-
     }
 
     @Test
@@ -178,6 +171,6 @@ class FunctionsTest {
     @Test
     void triIgnoreArg1() {
         assertThat(ignoreArg1(tri).apply(new Object(), "a", 1.0, 2.0f)).isEqualTo("a+1.0+2.0");
-
     }
+
 }
