1478e3c0d5769bda7e2847f3c0967d63df14bf0e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2021-04-23 09:36:05 +0200

    Introduced 'ReasonedPredicate'

---
+++ mihxil-functional/src/main/java/org/meeuw/functional/ReasonedPredicate.java
@@ -0,0 +1,96 @@
+package org.meeuw.functional;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.Predicate;
+
+/**
+ * A 'Reasoned' predicate, is a predicate, but besides the normal {@link #test(Object)} method it
+ * also supplies a {@link #testWithReason(Object)},which returns the {@link BooleanSupplier} {@link TestResult}, which besides
+ * the test result boolean also can give a reason for <em>why</em> the predicate evaluated as it did.
+ *
+ * This is useful e.g. when the predicate itself is combined from multiple other ones with all kind of (boolean) logic, and sometimes
+ * user may find it hard to understand _why_ the predicated evaluates as it does.
+ */
+public interface ReasonedPredicate<T> extends Predicate<T> {
+
+    /**
+     * Returns a reason and result for the predicate evaluation.
+     */
+    default TestResult testWithReason(T input) {
+        boolean applies = test(input);
+        if (applies) {
+            return new TestResult() {
+                @Override
+                public String toString() {
+                    return ReasonedPredicate.this.toString() + "(" + input + ")";
+                }
+
+                @Override
+                public boolean getAsBoolean() {
+                    return true;
+                }
+            };
+        } else {
+            return new TestResult() {
+                @Override
+                public String toString() {
+                    return "!" + ReasonedPredicate.this.toString() + "(" + input + ")";
+                }
+
+                @Override
+                public boolean getAsBoolean() {
+                    return false;
+                }
+            };
+        }
+    }
+
+    @Override
+    default boolean test(T input) {
+        return testWithReason(input).getAsBoolean();
+    }
+
+    /**
+     * Basicly wraps e boolean with a reason
+     */
+    interface TestResult extends BooleanSupplier {
+
+        default String getReason() {
+            return toString();
+        }
+
+        static TestResult testsFalse(String reason) {
+            return new TestResult() {
+                @Override
+                public boolean getAsBoolean() {
+                    return false;
+                }
+                @Override
+                public String toString() {
+                    return reason;
+                }
+            };
+        }
+
+        static TestResult testsTrue(String reason) {
+            return new TestResult() {
+                @Override
+                public boolean getAsBoolean() {
+                    return true;
+                }
+                @Override
+                public String toString() {
+                    return reason == null ? "tests true" : reason;
+                }
+                @Override
+                public String getReason() {
+                    return reason;
+                }
+            };
+        }
+
+        static TestResult of(boolean applies, String reason) {
+            return applies ? testsTrue(reason) : testsFalse(reason);
+        }
+    }
+}
---
+++ mihxil-functional/src/test/java/org/meeuw/functional/ReasonedPredicateTest.java
@@ -0,0 +1,88 @@
+package org.meeuw.functional;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ReasonedPredicateTest {
+
+    ReasonedPredicate<String> testString = new ReasonedPredicate<String>() {
+        @Override
+        public boolean test(String input) {
+            return "a".equals(input);
+        }
+        @Override
+        public String toString() {
+            return "equals('a')";
+
+        }
+    };
+
+    ReasonedPredicate<String> testString2 = new ReasonedPredicate<String>() {
+        @Override
+        public TestResult testWithReason(String input) {
+            String testString = "a";
+            boolean applies = false;
+            String reason;
+            if (input == null) {
+                reason = "input is null";
+            } else if (input.length() < testString.length()) {
+                reason = "input is too short";
+            } else if (input.length() > testString.length()) {
+                reason = "input is too long";
+            } else {
+                applies = testString.equals(input);
+                reason = (applies ? "input is equal " : "input is not equal ") + testString;
+            }
+
+            return TestResult.of(applies, reason);
+        }
+        @Override
+        public String toString() {
+            return "equals('a')";
+
+        }
+    };
+
+
+    @Test
+    public void test() {
+        assertThat(testString.test("b")).isFalse();
+        assertThat(testString.test("a")).isTrue();
+    }
+
+    @Test
+    public void testWithReason() {
+        assertThat(testString.testWithReason("b").getAsBoolean()).isFalse();
+        assertThat(testString.testWithReason("a").getAsBoolean()).isTrue();
+
+    }
+
+    @Test
+    public void reason() {
+        assertThat(testString.testWithReason("b").getReason()).isEqualTo("!equals('a')(b)");
+        assertThat(testString.testWithReason("a").getReason()).isEqualTo("equals('a')(a)");
+    }
+
+
+
+    @Test
+    public void test2() {
+        assertThat(testString2.test("b")).isFalse();
+        assertThat(testString2.test("a")).isTrue();
+    }
+
+    @Test
+    public void test2WithReason() {
+        assertThat(testString2.testWithReason("b").getAsBoolean()).isFalse();
+        assertThat(testString2.testWithReason("a").getAsBoolean()).isTrue();
+
+    }
+
+    @Test
+    public void reason2() {
+        assertThat(testString2.testWithReason("b").getReason()).isEqualTo("input is not equal a");
+        assertThat(testString2.testWithReason("a").getReason()).isEqualTo("input is equal a");
+    }
+
+}
