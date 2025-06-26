f6930db1b82009db2f64c9613eeec44bf5e622d1
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-06-26 17:14:18 +0200

    having closablesuppliers might be handy.

---
+++ mihxil-functional/src/main/java/org/meeuw/functional/CloseableSupplier.java
@@ -0,0 +1,17 @@
+package org.meeuw.functional;
+
+import java.util.function.Supplier;
+
+@FunctionalInterface
+public interface CloseableSupplier<T> extends Supplier<T>, AutoCloseable {
+
+    /**
+     * Closes the supplier, releasing any resources it holds.
+     * This method should be called when the supplier is no longer needed.
+     */
+    @Override
+    default void close() throws Exception {
+
+    }
+
+}
