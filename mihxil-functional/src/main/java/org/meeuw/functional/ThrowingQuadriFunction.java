ef836076ab194d32da40c07cdb6dff302eae3ca9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-06-29 16:35:08 +0200

    Also Quadri-..

---
+++ mihxil-functional/src/main/java/org/meeuw/functional/ThrowingQuadriFunction.java
@@ -0,0 +1,25 @@
+package org.meeuw.functional;
+
+import java.util.function.Consumer;
+
+import static org.meeuw.functional.Sneaky.sneakyThrow;
+
+
+/**
+ * An extension of {@link Consumer} that can throw exceptions too.
+ * @since 1.13
+ */
+@FunctionalInterface
+public interface ThrowingQuadriFunction<A, B, C, D, R, E extends Exception> extends QuadriFunction<A, B, C, D, R> {
+    @Override
+    default R apply(A a, B b, C c, D d) {
+        try {
+            return applyWithException(a, b, c, d);
+        } catch (final Exception e) {
+            return sneakyThrow(e);
+        }
+    }
+
+    R applyWithException(A a, B b, C c, D d) throws E;
+
+}
