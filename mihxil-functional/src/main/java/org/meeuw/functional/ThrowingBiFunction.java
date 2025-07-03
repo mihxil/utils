272f300521737b64f1f1805f7b2b16067b84e8db
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-07-03 21:21:34 +0200

    deploy with javadoc etc.

--- mihxil-functional/src/main/java/org/meeuw/functional/ThrowingBiFunction.java
+++ mihxil-functional/src/main/java/org/meeuw/functional/ThrowingBiFunction.java
@@ -8,6 +8,10 @@ import static org.meeuw.functional.Sneaky.sneakyThrow;
 /**
  * An extension of {@link BiFunction} that can throw exceptions too.
  * @since 1.13
+ * @param <A> the type of the first argument to the function
+ * @param <B> the type of the second argument to the function
+ * @param <R> the type of the result of the function
+ * @param <E> the type of the exception that can be thrown by this function
  */
 @FunctionalInterface
 public interface ThrowingBiFunction<A, B, R, E extends Exception> extends BiFunction<A, B, R> {
--- mihxil-functional/src/main/java/org/meeuw/functional/ThrowingSupplier.java
+++ mihxil-functional/src/main/java/org/meeuw/functional/ThrowingSupplier.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 /**
  * An extension of {@link Supplier} that can throw exceptions too.
  * @param <T> the type of results supplied by this supplier
+ * @param <E> the type of the exception that can be thrown by this supplier
  * @since 1.5
  */
 @FunctionalInterface
