32af3c69cee909363b7b832aea301973e70070c2
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-01-27 21:22:54 +0100

    javadoc.

--- mihxil-functional/src/main/java/org/meeuw/functional/ThrowingAutoCloseable.java
+++ mihxil-functional/src/main/java/org/meeuw/functional/ThrowingAutoCloseable.java
@@ -3,7 +3,9 @@ package org.meeuw.functional;
 import static org.meeuw.functional.Sneaky.sneakyThrow;
 
 /**
- * An extension of {@link AutoCloseable} that can throw exceptions too.
+ * An extension of {@link AutoCloseable} that has a typed exception.
+ * {@link #close()} will sneakily throw any exception thrown by {@link #closeThrows()}, which make it handy in try-with-resources.
+ *
  * @since 1.8
  * @param <E> the type of the exception that can be thrown
  */
@@ -21,7 +23,7 @@ public interface ThrowingAutoCloseable<E extends Exception> extends AutoCloseabl
 
     /**
      * Close, while allowing for an exception.
-     * @throws E if the operation somehow fails, it throws exceptions of this type
+     * @throws E if the operation somehow fails, it throws {@link Exception exceptions} of this type
      */
     void closeThrows() throws E;
 
