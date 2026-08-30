a9c65049091873fd84233f679f381d48a2e57ed1
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-08-30 20:48:58 +0200

    package info

---
+++ mihxil-functional/src/main/java/org/meeuw/functional/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains interfaces and implementations similar to e.g. {@link java.util.function.Function}
+ * (like {@link TriFunction}, {@link QuadriFunction}) and utilities.
+ */
+package org.meeuw.functional;
